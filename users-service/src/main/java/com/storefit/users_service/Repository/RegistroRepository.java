06636f95440a71773705ee7acf094dfdd6f19a7c
Benjamín <be.palmap@duocuc.cl>
2025-11-12 15:59:00 -0300

    Commit test

--- users-service/src/main/java/com/storefit/users_service/Repository/RegistroRepository.java
+++ users-service/src/main/java/com/storefit/users_service/Repository/RegistroRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
+    //repo
     boolean existsByUsuarioIgnoreCase(String usuario);
     Optional<Registro> findByUsuarioIgnoreCase(String usuario);
     Optional<Registro> findByRut(String rut);
